70c9e65c3ac3ca94cafdf603f1ccbbf2be8379a5
fuvx22 <voxngocphu@gmail.com>
2024-03-18 08:46:35 +0700

    for phuc map

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Dashboard from './pages/Dashboard';
 import Login from './components/Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
+import Userboard from './pages/Userboard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/userboard" element={<Userboard />} />
       </Routes>
     </Router>
   );
