c27bbaa76a7fe01b98edcd4c30fe891f00089b4f
Hoang Phuc <hoangphuc1996vnvn@gmail.com>
2024-03-19 13:27:39 +0700

    70% authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from 'react';
-import { loginAPI } from '../apis';
+import React, { useState } from "react";
+import { loginAPI } from "../apis";
+import { toast } from "react-toastify";
 
 function Login() {
-  const [id, setId] = useState('');
-  const [password, setPassword] = useState('');
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
   const [isShowPass, setIsShowPass] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(null);
   const handleLogin = async () => {
-    if (!id || !password) return;
-    let res = await loginAPI(id, password);
-    alert(JSON.stringify(res.data));
+    setIsLoading(true);
+    try {
+      if (!id || !password) return;
+      let res = await loginAPI(id, password);
+
+      if (res.status == 200) {
+        setIsLoading(false);
+        localStorage.setItem("user-token", JSON.stringify(res.data));
+        toast.success("Đăng nhập thành công");
+      }
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.response.data.error);
+    }
   };
 
   return (
     <div className="login-container col-xs-10 col-sm-6 col-lg-4">
-      <div className="title">Log in</div>
+      <div className="title">Đăng Nhập</div>
       <div className="text">Student ID</div>
       <input
         value={id}
@@ -22,26 +34,37 @@ function Login() {
         type="text"
         placeholder="Student ID..."
       />
+      <div className="text">Password</div>
       <div className="password-input">
         <input
           value={password}
           onChange={(event) => setPassword(event.target.value)}
-          type={isShowPass ? 'text' : 'password'}
+          type={isShowPass ? "text" : "password"}
           placeholder="Password..."
         />
         <i
-          className={isShowPass ? 'fa-solid fa-eye' : 'fa-solid fa-eye-slash'}
+          className={isShowPass ? "fa-solid fa-eye" : "fa-solid fa-eye-slash"}
           onClick={() => setIsShowPass(!isShowPass)}
         ></i>
       </div>
+
       <button
-        disabled={id && password ? false : true}
-        className={id && password ? 'active' : ''}
+        disabled={!id || !password || isLoading}
+        className={id && password && !isLoading ? "active" : ""}
         onClick={() => handleLogin()}
       >
-        Login
+        <div className="d-flex justify-content-center align-items-center gap-3 position-relative">
+          <span className="fs-5">Login</span>
+          {isLoading && (
+            <div
+              className="spinner-border text-primary position-absolute end-0"
+              role="status"
+            >
+              <span className="visually-hidden"></span>
+            </div>
+          )}
+        </div>
       </button>
-      <img src="" alt="..." className="img-circle img-responsive mt-3"></img>
     </div>
   );
 }
