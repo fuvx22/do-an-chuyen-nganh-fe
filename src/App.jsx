070a5d7004d8037c4d68f03fdcfbfa96f28ba291
fuvx22 <voxngocphu@gmail.com>
2024-03-25 09:44:07 +0700

    fix file App

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-
-import Dashboard from './pages/Dashboard';
-import Login from './components/Login';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React from 'react';
-import Userboard from './pages/Userboard';
-import ScheduleTable from './pages/ScheduleTable';
-import CourseManage from './pages/CourseManage';
+import Dashboard from "./pages/Dashboard";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import Userboard from "./pages/Userboard";
+import ScheduleTable from "./pages/ScheduleTable";
+import CourseManage from "./pages/CourseManage";
+import { UserProvider } from "./context/userContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/userboard" element={<Userboard />} />
-        <Route path="/schedule" element={<ScheduleTable />} />
-        <Route path="/course-manage" element={<CourseManage />} />
-      </Routes>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/userboard" element={<Userboard />} />
+          <Route path="/schedule" element={<ScheduleTable />} />
+          <Route path="/course-manage" element={<CourseManage />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
 }
 
