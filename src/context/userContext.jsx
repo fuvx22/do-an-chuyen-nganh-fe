30c40116ebbd789659f915c61736d2d241d236d8
Hoang Phuc <hoangphuc1996vnvn@gmail.com>
2024-03-28 13:37:34 +0700

    Fixed: Reload Authentication, fetch Data user

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -9,23 +9,32 @@ export const UserContext = createContext();
 export const UserProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
   const [error, setError] = useState(null);
+  let token = JSON.parse(localStorage.getItem("user-token"));
+  const login = async (tk) => {
+    token = tk;
+    await fetchUserData(token);
+  };
+  const fetchUserData = async (token) => {
+    try {
+      const response = await fetchUserAPI(token);
+      setUserData(response.data);
+      // console.log(response.data);
+      setError(null);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  };
   useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const response = await fetchUserAPI();
-        setUserData(response.data);
-        // console.log(response.data);
-        setError(null);
-      } catch (error) {
-        console.log(error);
-        setError(error);
-      }
-    };
-    fetchUserData();
+    if (!token) {
+      return;
+    } else {
+      fetchUserData(token);
+    }
   }, []);
 
   return (
-    <UserContext.Provider value={{ userData, error }}>
+    <UserContext.Provider value={{ userData, error, login }}>
       {children}
     </UserContext.Provider>
   );
