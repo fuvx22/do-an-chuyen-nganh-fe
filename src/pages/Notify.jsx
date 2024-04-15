1388131d9324906ae80cc0d947f729f11476cfa2
fuvx22 <voxngocphu@gmail.com>
2024-04-15 08:54:11 +0700

    add load effect

--- src/pages/Notify.jsx
+++ src/pages/Notify.jsx
@@ -2,14 +2,12 @@ import React, { useState, useContext, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { UserContext } from "../context/userContext";
 import { useParams, useNavigate } from "react-router-dom";
-import { findOneNotifyAPI } from "../apis"
+import { findOneNotifyAPI } from "../apis";
 import { toast } from "react-toastify";
 
-
 function Notify() {
-
   const { userData } = useContext(UserContext);
-  const [notify, setNotify ] = useState({});
+  const [notify, setNotify] = useState();
   const { id } = useParams();
   const navigate = useNavigate();
   const token = JSON.parse(localStorage.getItem("user-token"));
@@ -18,31 +16,52 @@ function Notify() {
     if (!token) {
       navigate("/");
       return;
-    } 
+    }
     const fetchData = async () => {
       try {
-          const getNotify = await findOneNotifyAPI(id);
-          setNotify(getNotify);
-        } catch (error) {
-          toast.error("Không thể kết nối đến server");
-        }
-      };
-      fetchData();
-  },[id])
+        const getNotify = await findOneNotifyAPI(id);
+        setNotify(getNotify);
+      } catch (error) {
+        toast.error("Không thể kết nối đến server");
+      }
+    };
+    fetchData();
+  }, [id]);
 
-
-
-  return <div className="col-12 col-sm-10 col-md-8 m-auto">
-    <Navbar user={userData}/>
-    <div className="contaner mt-3">
-      <div className="d-flex justify-content-between align-items-center">
-        <h2>{notify?.title}</h2>
-        <h6>Ngày tạo: {new Date(notify?.createAt).toLocaleDateString()}</h6>
-      </div>
-      <div className="mt-5" dangerouslySetInnerHTML={{ __html: notify?.content }}>
+  return (
+    <div className="col-12 col-sm-10 col-md-8 m-auto">
+      <Navbar user={userData} />
+      <div className="contaner mt-3" style={{ minHeight: "100vh" }}>
+        {notify ? (
+          <div>
+            <div className="d-flex justify-content-between align-items-center">
+              <h2>{notify?.title}</h2>
+              <h6>
+                Ngày tạo: {new Date(notify?.createAt).toLocaleDateString()}
+              </h6>
+            </div>
+            <div
+              className="mt-5"
+              dangerouslySetInnerHTML={{ __html: notify?.content }}
+            ></div>
+          </div>
+        ) : (
+          <div
+            style={{ height: "100vh" }}
+            className="d-flex justify-content-center align-items-center"
+          >
+            <div
+              className="spinner-border text-primary"
+              style={{ width: "4.5rem", height: "4.5rem" }}
+              role="status"
+            >
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        )}
       </div>
     </div>
-  </div>
+  );
 }
 
 export default Notify;
