6c4a6e4a510c459d8f91af1738494d6ead2bea86
HoangPhuc13 <nguyenhoangphuc9074@gmail.com>
2024-04-18 22:55:56 +0700

    done modal for courseSchedule

--- src/pages/CourseScheduleManage.jsx
+++ src/pages/CourseScheduleManage.jsx
@@ -15,6 +15,20 @@ import {
 import { semesterErrorClassify } from "../utils/validator";
 import { UserContext } from "../context/userContext";
 import { useNavigate } from "react-router-dom";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    width: "max-content",
+    height: "600px",
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
 
 let indexToEdit = -1;
 
@@ -23,8 +37,15 @@ function CourseScheduleManage() {
   const [courses, setCourses] = useState([]);
   const [instructors, setInstructors] = useState([]);
   const [semesters, setSemesters] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedCourseSchedule, setSelectedCourseSchedule] = useState(null);
   const { userData } = useContext(UserContext);
   const token = JSON.parse(localStorage.getItem("user-token"));
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -87,6 +108,8 @@ function CourseScheduleManage() {
         }
         const newCourseSchedule = {
           dayOfWeek: dayOfWeek,
+          startPeriod: startPeriod,
+          endPeriod: endPeriod,
           period: periodArray,
           group: group,
           roomNumber: roomNumber,
@@ -121,7 +144,7 @@ function CourseScheduleManage() {
     setDayOfWeek(courseSchedules[index].dayOfWeek);
     //setPeriod(courseSchedules[index].period.join(''));
     setStartPeriod(courseSchedules[index].period[0].toString());
-    setEndPeriod( courseSchedules[index].period[courseSchedules[index].period.length - 1].toString());
+    setEndPeriod(courseSchedules[index].period[courseSchedules[index].period.length - 1].toString());
     setGroup(courseSchedules[index].group);
     setRoomNumber(courseSchedules[index].roomNumber);
     setMaxQuantity(courseSchedules[index].maxQuantity);
@@ -132,34 +155,34 @@ function CourseScheduleManage() {
 
   const confirmEdit = async () => {
     //const periodArray = period.toString().split('').map(Number);
+    console.log(selectedCourseSchedule)
     const periodArray = [];
-        for (let i = parseInt(startPeriod); i <= parseInt(endPeriod); i++) {
+        for (let i = parseInt(selectedCourseSchedule.startPeriod); i <= parseInt(selectedCourseSchedule.endPeriod); i++) {
           periodArray.push(i);
         }
-    if (
-      dayOfWeek &&
-      startPeriod != "default" && startPeriod != "" &&
-      endPeriod != "default" && endPeriod != "" &&
-      group &&
-      roomNumber &&
-      maxQuantity &&
-      courses != "default" && courses != "" &&
-      instructors != "default" && instructors != ""&&
-      semesters != "default" && semesters != "" ) {
-      
-      courseSchedules[indexToEdit].dayOfWeek = dayOfWeek;
-      courseSchedules[indexToEdit].period = periodArray;
-      courseSchedules[indexToEdit].group = group;
-      courseSchedules[indexToEdit].roomNumber = roomNumber;
-      courseSchedules[indexToEdit].maxQuantity = maxQuantity;
-      courseSchedules[indexToEdit].courseId = course;
-      courseSchedules[indexToEdit].instructorId = instructor;
-      courseSchedules[indexToEdit].semesterId = semester;
+        console.log(periodArray)
 
+        const { courseId, instructorId, semesterId, dayOfWeek, startPeriod, endPeriod, group, roomNumber, maxQuantity} = selectedCourseSchedule;
+    if (
+        courseId != "" ||
+        instructorId != "" ||
+        semesterId != "" ||
+        dayOfWeek != "" ||
+        startPeriod != "" ||
+        endPeriod != "" ||
+        group != "" ||
+        roomNumber != "" ||
+        maxQuantity != "" 
+    ) {
+      
       try {
-        await editCourseScheduleAPI(courseSchedules[indexToEdit]);
+        selectedCourseSchedule.period = periodArray
+        console.log(selectedCourseSchedule.period)
+        await editCourseScheduleAPI(selectedCourseSchedule);
+        const updatedCourseSchedule = await fetchCourseSchedulesAPI(token);
+        setCourseSchedules(updatedCourseSchedule);
         toast.success("Cập nhật thành công");
-        cancelActivity();
+        closeModal()
       } catch (error) {
         toast.error(semesterErrorClassify(error), {
           position: "top-center",
@@ -175,6 +198,11 @@ function CourseScheduleManage() {
       courseScheduleIdRef.current.focus();
     }
   }
+
+  const handleEditModal = async (courseSchedule) => {
+    setSelectedCourseSchedule(courseSchedule)
+    setIsOpen(true);
+  };
 
   const handleDeleteCourseSchedule = async (index) => {
     try {
@@ -206,7 +234,14 @@ function CourseScheduleManage() {
   };
 
   
-
+  const generatePeriod = () => {
+    const options = [];
+    for (let i = 1; i <= 10; i++) {
+      options.push(<option key={i} value={i}>{i}</option>);
+    }
+    return options;
+  };
+  
   
 
   
@@ -293,23 +328,7 @@ function CourseScheduleManage() {
             <option value="Thứ 7">Thứ 7</option>
           </select>
         </div>
-        {/* <div className="control-item">
-          <label htmlFor="courseCredit" className="form-label">
-            Tiết học
-          </label>
-          <input
-            value={period}
-            onChange={(e) => {
-              setPeriod(e.target.value);
-            }}
-            type="number"
-            className="form-control"
-            id="period"
-          />
-        </div> */}
         <div className="control-item">
-          
-          
           <div className="d-flex justify-content-between">
             <div style={{flex: "0 0 45%"}}>
               <label htmlFor="" className="form-label">Tiết bắt đầu</label>
@@ -321,16 +340,7 @@ function CourseScheduleManage() {
                 onChange={(e) => setStartPeriod(e.target.value)}
               >
                 <option value="default">Chọn tiết bắt đầu</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {generatePeriod()}
               </select>
             </div>
             <div style={{flex: "0 0 45%"}}>
@@ -343,16 +353,7 @@ function CourseScheduleManage() {
                 onChange={(e) => setEndPeriod(e.target.value)}
               >
                 <option value="default">Chọn tiết kết thúc</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                {generatePeriod()}
               </select>
             </div>
           </div>
@@ -404,9 +405,9 @@ function CourseScheduleManage() {
         <div className="control-item d-flex gap-2">
           <button
             className="btn btn-primary"
-            onClick={() => (isEdit ? confirmEdit() : handleAddCourseSchedule())}
+            onClick={() => handleAddCourseSchedule()}
           >
-            {!isEdit ? "Thêm mới" : "Cập nhật "}
+            Thêm mới
           </button>
           <button
             className="btn btn-outline-danger"
@@ -450,28 +451,252 @@ function CourseScheduleManage() {
                 
                 
                 
-                <td>
+                <td style={{ minWidth: "110px", textAlign: "right" }}>
                   <button
-                    onClick={() => handleEditCourseSchedule(index)}
-                    type="button"
-                    className="btn btn-primary btn-sm"
+                    onClick={() => {
+                      handleEditModal(c);
+                    }}
+                    className="btn btn-primary"
                   >
                     <i className="fas fa-edit"></i>
                   </button>
-                </td>
-                <td>
                   <button
-                    onClick={() => handleDeleteCourseSchedule(index)}
-                    type="button"
-                    className="btn btn-danger btn-sm"
-                  >
-                    <i className="fas fa-trash"></i>
-                  </button>
+                      onClick={() => handleDeleteCourseSchedule(index)}
+                      className="btn btn-danger mx-1"
+                    >
+                      <i className="fas fa-trash"></i>
+                    </button>
                 </td>
+                <td></td>
               </tr>
             ))}
           </tbody>
         </table>
+
+
+        <Modal
+          isOpen={isOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <div className="modal-header">
+            <h4 className="modal-title">Chỉnh sửa </h4>
+          </div>
+
+          <div className="modal-body">
+              <form>
+              <div className="mb-3">
+                <label htmlFor="courseId" className="form-label">
+                  Môn học
+                </label>
+                <select
+                  className="form-select"
+                  id="type"
+                  value={selectedCourseSchedule?.courseId || ""}
+                  onChange={(e) =>
+                    setSelectedCourseSchedule({
+                      ...selectedCourseSchedule,
+                      courseId: e.target.value,
+                    })
+                  }
+                >
+                  <option value="">Chọn nhóm ngành</option>
+                  {courses.map((course) => (
+                    <option key={course._id} value={course._id}>
+                      {course.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="instructorId" className="form-label">
+                  Giảng viên
+                </label>
+                <select
+                  className="form-select"
+                  id="type"
+                  value={selectedCourseSchedule?.instructorId || ""}
+                  onChange={(e) =>
+                    setSelectedCourseSchedule({
+                      ...selectedCourseSchedule,
+                      instructorId: e.target.value,
+                    })
+                  }
+                >
+                  <option value="">Chọn giảng viên</option>
+                  {instructors.map((instructor) => (
+                    <option key={instructor._id} value={instructor._id}>
+                      {instructor.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="courseId" className="form-label">
+                  Học kỳ
+                </label>
+                <select
+                  className="form-select"
+                  id="type"
+                  value={selectedCourseSchedule?.semesterId || ""}
+                  onChange={(e) =>
+                    setSelectedCourseSchedule({
+                      ...selectedCourseSchedule,
+                      semesterId: e.target.value,
+                    })
+                  }
+                >
+                  <option value="">Chọn học kỳ</option>
+                  {semesters.map((semester) => (
+                    <option key={semester._id} value={semester._id}>
+                      {semester.semesterName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="dayOfWeek" className="form-label">
+                  Ngày học
+                </label>
+                <select
+                  className="form-select"
+                  id="type"
+                  value={selectedCourseSchedule?.dayOfWeek || ""}
+                  onChange={(e) =>
+                    setSelectedCourseSchedule({
+                      ...selectedCourseSchedule,
+                      dayOfWeek: e.target.value,
+                    })
+                  }
+                >
+                  <option value="default">Chọn ngày học</option>
+                  <option value="Thứ 2">Thứ 2</option>
+                  <option value="Thứ 3">Thứ 3</option>
+                  <option value="Thứ 4">Thứ 4</option>
+                  <option value="Thứ 5">Thứ 5</option>
+                  <option value="Thứ 6">Thứ 6</option>
+                  <option value="Thứ 7">Thứ 7</option>
+                </select>
+              </div>
+              
+              <div className="mb-3">
+                <div className="d-flex justify-content-between">
+                  <div style={{flex: "0 0 45%"}}>
+                    <label htmlFor="" className="form-label">Tiết bắt đầu</label>
+                    <select
+                      className="form-select"
+                      id="startPeriod"
+                      value={selectedCourseSchedule?.startPeriod || ""}
+                      onChange={(e) =>
+                        setSelectedCourseSchedule({
+                          ...selectedCourseSchedule,
+                          startPeriod: e.target.value,
+                        })
+                      }
+                    >period[courseSchedules[index].period.length - 1]
+                      <option value="default">Chọn tiết bắt đầu</option>
+                      {generatePeriod()}
+                    </select>
+                  </div>
+                  <div style={{flex: "0 0 45%"}}>
+                    <label htmlFor="" className="form-label">Tiết kết thúc</label>
+                    <select
+                      className="form-select"
+                      id="endPeriod"
+                      value={selectedCourseSchedule?.endPeriod || ""}
+                      onChange={(e) =>
+                        setSelectedCourseSchedule({
+                          ...selectedCourseSchedule,
+                          endPeriod: e.target.value,
+                        })
+                      }
+                    >
+                      <option value="default">Chọn tiết kết thúc</option>
+                      {generatePeriod()}
+                    </select>
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="group" className="form-label">
+                  Nhóm lớp
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="group"
+                  value={selectedCourseSchedule?.group || ""}
+                  onChange={(e) =>
+                    setSelectedCourseSchedule({
+                      ...selectedCourseSchedule,
+                      group: e.target.value,
+                    })
+                  }
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="roomNumber" className="form-label">
+                  Phòng học
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="roomNumber"
+                  value={selectedCourseSchedule?.roomNumber || ""}
+                  onChange={(e) =>
+                    setSelectedCourseSchedule({
+                      ...selectedCourseSchedule,
+                      roomNumber: e.target.value,
+                    })
+                  }
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="maxQuantity" className="form-label">
+                  Sĩ số
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="maxQuantity"
+                  value={selectedCourseSchedule?.maxQuantity || ""}
+                  onChange={(e) =>
+                    setSelectedCourseSchedule({
+                      ...selectedCourseSchedule,
+                      maxQuantity: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              
+              </form>
+          </div>
+
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={closeModal}
+            >
+              Đóng
+            </button>
+            <button
+              type="button"
+              onClick={confirmEdit}
+              className="btn btn-primary mx-1"
+            >
+              Lưu thay đổi
+            </button>
+          </div>
+        </Modal>
+
       </div>
     </div>
   );
